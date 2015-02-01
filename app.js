fd8dd4cef39d1d78b9ee0731b6287df32c5ad6c0
Albert Arvidsson <albert.arvidsson@gmail.com>
2015-02-01 13:04:20 +0100

    Version 2

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 (function() {
 
-  var streaming = false,
-      video        = document.querySelector('#video'),
-      canvas_left  = document.querySelector('#canvas_left'),
-      canvas_right = document.querySelector('#canvas_right'),
-      ctx_left     = canvas_left.getContext('2d'),
-      ctx_right    = canvas_right.getContext('2d'),
-      // photo        = document.querySelector('#photo'),
-      // startbutton  = document.querySelector('#startbutton'),
-      width = 320,
-      height = 0;
+  var streaming = false;
+  var video        = document.querySelector('#middle video');
+  var canvas_left  = document.querySelector('#left canvas');
+  var canvas_middle = document.createElement('canvas');
+  var canvas_right = document.querySelector('#right canvas');
+  var ctx_left     = canvas_left.getContext('2d');
+  var ctx_middle   = canvas_middle.getContext('2d');
+  var ctx_right    = canvas_right.getContext('2d');
+  var snapshots_left   = document.querySelector('#left   .snapshots');
+  var snapshots_middle = document.querySelector('#middle .snapshots');
+  var snapshots_right  = document.querySelector('#right  .snapshots');
+  // var photo        = document.querySelector('#photo');
+  // var startbutton  = document.querySelector('#startbutton');
+  var width = 320;
+  var height = 0;
 
   navigator.getMedia = ( navigator.getUserMedia ||
                          navigator.webkitGetUserMedia ||
@@ -42,76 +47,61 @@
       video.setAttribute('height', height);
       canvas_left.setAttribute('width', width);
       canvas_left.setAttribute('height', height);
+      canvas_middle.setAttribute('width', width);
+      canvas_middle.setAttribute('height', height);
+      canvas_right.setAttribute('width', width);
+      canvas_right.setAttribute('height', height);
       streaming = true;
     }
   }, false);
 
-  function takepicture() {
-    canvas_left.width = width;
-    canvas_left.height = height;
+  setInterval(function () {
+    render();
+  }, 30);
+
+  function render() {
     ctx_left.drawImage(video, 0, 0, width, height);
-    canvas_right.width = width;
-    canvas_right.height = height;
+    ctx_middle.drawImage(video, 0, 0, width, height);
     ctx_right.drawImage(video, 0, 0, width, height);
 
-    var flipdata_left  = flipLeft(ctx_left.getImageData(0,0,width,height));
-    var flipdata_right = flipRight(ctx_right.getImageData(0,0,width,height));
+    var flipdata_left  = flip(ctx_left.getImageData(0,0,width,height), 0, width/2);
+    var flipdata_right = flip(ctx_right.getImageData(0,0,width,height), width/2, width);
 
     ctx_left.putImageData(flipdata_left,   0, 0);
     ctx_right.putImageData(flipdata_right, 0, 0);
-
-    var data_left = canvas_left.toDataURL('image/png');
-    photo_left.setAttribute('src', data_left);
-    var data_right = canvas_right.toDataURL('image/png');
-    photo_right.setAttribute('src', data_right);
   }
 
-  // startbutton.addEventListener('click', function(ev){
-  //     takepicture();
-  //   ev.preventDefault();
-  // }, false);
+  function takepicture() {
+    var photo_left = document.createElement('img');
+    var data_left = canvas_left.toDataURL('image/png');
+    photo_left.setAttribute('src', data_left);
+    snapshots_left.insertBefore(photo_left, snapshots_left.firstChild);
+
+    var photo_middle = document.createElement('img');
+    var data_middle = canvas_middle.toDataURL('image/png');
+    photo_middle.setAttribute('src', data_middle);
+    snapshots_middle.insertBefore(photo_middle, snapshots_middle.firstChild);
+
+    var photo_right = document.createElement('img');
+    var data_right = canvas_right.toDataURL('image/png');
+    photo_right.setAttribute('src', data_right);
+    snapshots_right.insertBefore(photo_right, snapshots_right.firstChild);
+  }
 
   document.body.addEventListener('keydown', onKeyPress);
 
   function onKeyPress(event) {
     switch (event.keyCode) {
-      case 32: takepicture(); break;
+      case 32: event.preventDefault(); takepicture(); break;
     }
   }
 
-  function flipLeft(imageData) {
+  function flip(imageData, from, to) {
     var data = imageData.data;
     var width = imageData.width;
     var height = imageData.height;
     for(var y = 0; y < height; y++) {
-        for(var x = 0; x < width / 2; x++) { // divide by 2 to only loop through the left half of the image.
-            var offset = ((width* y) + x) * 4; // Pixel origin
-
-            // Get pixel
-            var r = data[offset];
-            var g = data[offset + 1];
-            var b = data[offset + 2];
-            var a = data[offset + 3];
-
-            // Calculate how far to the right the mirrored pixel is
-            var mirrorOffset = (width - (x * 2)) * 4;
-
-            // Get set mirrored pixel's colours 
-            data[offset + mirrorOffset] = r;
-            data[offset + 1 + mirrorOffset] = g;
-            data[offset + 2 + mirrorOffset] = b;
-            data[offset + 3 + mirrorOffset] = a;
-        }
-    }
-    return imageData;
-  }
-
-  function flipRight(imageData) {
-    var data = imageData.data;
-    var width = imageData.width;
-    var height = imageData.height;
-    for(var y = 0; y < height; y++) {
-        for(var x = width/2; x < width; x++) { // divide by 2 to only loop through the left half of the image.
+        for(var x = from; x < to; x++) { // divide by 2 to only loop through the left half of the image.
             var offset = ((width* y) + x) * 4; // Pixel origin
 
             // Get pixel
