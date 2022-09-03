5c92fcdb5dd82ffb11184479b9a9b84662a57024
Albert Arvidsson <albert.arvidsson@gmail.com>
2022-09-03 12:08:34 +0200

    Use mediaDevices.getUserMedia instead and refactor
    Update this really old project to try and fix Safari support.
    
    Use the newer promise based api navigator.mediaDevices.getUserMedia().
    https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
    
    Refactor to use script type module and class syntax instead.

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-(function() {
+let streaming = false;
+const video = document.createElement('video');
+const width = 320;
+const height = 240;
 
-  var streaming = false;
-  var video        = document.createElement('video');
-  var width = 320;
-  var height = 240;
-
-  function StreamContainer() {
+class StreamContainer {
+  constructor() {
     this.el = document.body;
     this.streams = [];
   }
-  StreamContainer.prototype.append = function (stream) {
+  append(stream) {
     this.streams.push(stream);
     this.el.appendChild(stream.el);
   };
+}
 
-  function Stream(id, video) {
+class Stream {
+  constructor(id, video) {
     this.el = document.createElement('div');
     this.el.classList.add('Stream');
     this.el.id = id;
@@ -30,45 +31,45 @@
       this.video = video;
       this.video.setAttribute('width', width);
 
-      var line = document.createElement('div');
+      const line = document.createElement('div');
       line.classList.add('Line');
       this.el.appendChild(line);
-
-      // this.el.appendChild(this.video);
     }
 
     this.snapshots = document.createElement('div');
     this.snapshots.classList.add('SnapshotContainer');
     this.el.appendChild(this.snapshots);
   }
-  Stream.prototype.prependSnapshot = function (snapshot) {
+  prependSnapshot(snapshot) {
     this.snapshots.insertBefore(snapshot, this.snapshots.firstChild);
-  };
-  Stream.prototype.takeSnapshot = function () {
-    var data = this.canvas.toDataURL('image/png');
-    var snapshot = new Snapshot(data);
+  }
+  takeSnapshot() {
+    const data = this.canvas.toDataURL('image/png');
+    const snapshot = new Snapshot(data);
     this.prependSnapshot(snapshot.el);
-  };
+  }
+}
 
-  function Snapshot(data) {
+class Snapshot {
+  constructor(data) {
     this.el = document.createElement('div');
     this.el.classList.add('Snapshot');
 
-    this.closeButton = new CloseButton(onClose.bind(this));
-    this.saveButton = new SaveButton(data);
-    this.photo = new Photo(data);
-
-    function onClose() {
+    const onClose = () => {
       this.el.parentNode.removeChild(this.el);
     }
-    function onSave() {
-    }
+
+    this.closeButton = new CloseButton(onClose);
+    this.saveButton = new SaveButton(data);
+    this.photo = new Photo(data);
     this.el.appendChild(this.closeButton.el);
     this.el.appendChild(this.saveButton.el);
     this.el.appendChild(this.photo.el);
   }
+}
 
-  function CloseButton(onClose) {
+class CloseButton {
+  constructor(onClose) {
     this.el = document.createElement('div');
     this.el.classList.add('Button');
     this.el.classList.add('CloseButton');
@@ -76,8 +77,10 @@
     this.el.innerHTML = "&times;";
     this.el.addEventListener('click', onClose);
   }
+}
 
-  function SaveButton(href) {
+class SaveButton {
+  constructor(href) {
     this.el = document.createElement('a');
     this.el.classList.add('Button');
     this.el.classList.add('SaveButton');
@@ -87,21 +90,22 @@
     this.el.setAttribute('title', 'Click to download');
     this.el.innerHTML = '&starf;';
   }
+}
 
-  function Photo(src) {
+class Photo {
+  constructor(src) {
     this.el = document.createElement('img');
     this.el.setAttribute('src', src);
   }
+}
 
-  navigator.getMedia = ( navigator.getUserMedia ||
-                         navigator.webkitGetUserMedia ||
-                         navigator.mozGetUserMedia ||
-                         navigator.msGetUserMedia);
+const streamContainer = new StreamContainer();
 
-  var explanation = document.createElement('div');
+function init() {
+  const explanation = document.createElement('div');
   explanation.classList.add('Explanation');
 
-  if (navigator.getMedia) {
+  if (navigator.mediaDevices.getUserMedia) {
     explanation.innerHTML = 'You need to allow this page to use your camera';
     document.body.appendChild(explanation);
   } else {
@@ -111,25 +115,17 @@
     return;
   }
 
-  var streamContainer = new StreamContainer();
-
   streamContainer.append(new Stream('left'));
   streamContainer.append(new Stream('right'));
   streamContainer.append(new Stream('middle', video));
 
-  navigator.getMedia(
-    {
-      video: true,
-      audio: false
-    },
-    function(stream) {
-      if (navigator.mozGetUserMedia) {
-        video.mozSrcObject = stream;
-      } else {
-        var vendorURL = window.URL || window.webkitURL;
-        video.srcObject = stream
-      }
-      video.play();
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then(
+    (stream) => {
+      video.srcObject = stream
+      video.onloadedmetadata = () => {
+        video.play();
+      };
+
       explanation.innerHTML = 'Position your face so that the red line ' +
         'divides it in half. ' +
         'Then press <code>&lt;SPACE&gt;</code> to take a snapshot';
@@ -141,12 +137,12 @@
     }
   );
 
-  video.addEventListener('canplay', function(ev){
+  video.addEventListener('canplay', function(ev) {
     if (!streaming) {
       video.setAttribute('width', video.videoWidth * 2);
       video.setAttribute('height', video.videoHeight * 2);
 
-      streamContainer.streams.forEach(function (stream) {
+      streamContainer.streams.forEach(function(stream) {
         stream.canvas.setAttribute('width', width);
         stream.canvas.setAttribute('height', height);
       });
@@ -155,65 +151,66 @@
     }
   }, false);
 
-  setInterval(function () {
+  setInterval(function() {
     render();
   }, 30);
+}
 
-  function render() {
-    var left = streamContainer.streams[0].context;
-    left.drawImage(video, 0, 0, width, height);
+function render() {
+  const left = streamContainer.streams[0].context;
+  left.drawImage(video, 0, 0, width, height);
 
-    var middle = streamContainer.streams[2].context;
-    middle.drawImage(video, 0, 0, width, height);
+  const middle = streamContainer.streams[2].context;
+  middle.drawImage(video, 0, 0, width, height);
 
-    var right  = streamContainer.streams[1].context;
-    right.drawImage(video, 0, 0, width, height);
+  const right = streamContainer.streams[1].context;
+  right.drawImage(video, 0, 0, width, height);
 
-    var flipdata_left  = flip(left.getImageData(0,0,width,height), 0, width/2);
-    var flipdata_right = flip(right.getImageData(0,0,width,height), width/2, width);
+  const flipdata_left = flip(left.getImageData(0, 0, width, height), 0, width / 2);
+  const flipdata_right = flip(right.getImageData(0, 0, width, height), width / 2, width);
 
-    left.putImageData(flipdata_left,   0, 0);
-    right.putImageData(flipdata_right, 0, 0);
+  left.putImageData(flipdata_left, 0, 0);
+  right.putImageData(flipdata_right, 0, 0);
+}
+
+function takepicture() {
+  streamContainer.streams.forEach(function(stream) {
+    stream.takeSnapshot();
+  });
+}
+
+function onKeydown(event) {
+  switch (event.keyCode) {
+    case 32: event.preventDefault(); takepicture(); break;
   }
+}
 
-  function takepicture() {
-    streamContainer.streams.forEach(function (stream) {
-      stream.takeSnapshot();
-    });
-  }
-
-  function onKeydown(event) {
-    switch (event.keyCode) {
-      case 32: event.preventDefault(); takepicture(); break;
-    }
-  }
-
-  function flip(imageData, from, to) {
-    var data = imageData.data;
-    var width = imageData.width;
-    var height = imageData.height;
-    // http://stackoverflow.com/a/13933017
-    for(var y = 0; y < height; y++) {
-      for(var x = from; x < to; x++) { // divide by 2 to only loop through the left half of the image.
-        var offset = ((width* y) + x) * 4; // Pixel origin
+function flip(imageData, from, to) {
+  const data = imageData.data;
+  const width = imageData.width;
+  const height = imageData.height;
+  // http://stackoverflow.com/a/13933017
+  for (let y = 0; y < height; y++) {
+    for (let x = from; x < to; x++) { // divide by 2 to only loop through the left half of the image.
+      const offset = ((width * y) + x) * 4; // Pixel origin
 
       // Get pixel
-      var r = data[offset];
-      var g = data[offset + 1];
-      var b = data[offset + 2];
-      var a = data[offset + 3];
+      const r = data[offset];
+      const g = data[offset + 1];
+      const b = data[offset + 2];
+      const a = data[offset + 3];
 
       // Calculate how far to the right the mirrored pixel is
-      var mirrorOffset = (width - (x * 2)) * 4;
+      const mirrorOffset = (width - (x * 2)) * 4;
 
       // Get set mirrored pixel's colours 
       data[offset + mirrorOffset] = r;
       data[offset + 1 + mirrorOffset] = g;
       data[offset + 2 + mirrorOffset] = b;
       data[offset + 3 + mirrorOffset] = a;
-      }
     }
-    return imageData;
   }
+  return imageData;
+}
 
-})();
+init()
